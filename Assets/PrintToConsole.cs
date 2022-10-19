4ce407d540b9eeb5dfde0c5274cd38f1c7af6755
Rasmus Wulff Jensen <rasmus.wulff.jensen@outlook.com>
2022-10-19 15:25:15 +0200

    Done Experimenting

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Ate, to many donuts");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
