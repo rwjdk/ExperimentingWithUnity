f5bc0ebad961642425058b5a65971f05b56ea551
Rasmus Wulff Jensen <rasmus.wulff.jensen@outlook.com>
2022-11-10 10:58:18 +0100

    Start of Tower Defense Game . WayPoint Editor finished

---
+++ Tower Defense/Assets/Scripts/WayPoint/WayPoint.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+namespace WayPoint
+{
+    public class WayPoint : MonoBehaviour
+    {
+        [SerializeField] private Vector3[] _points;
+
+        private bool _gameStarted;
+
+        public Vector3[] Points => _points;
+        public Vector3 CurrentPosition { get; private set; }
+
+        private void Start()
+        {
+            _gameStarted = true;
+            CurrentPosition = transform.position;
+        }
+
+        private void Update()
+        {
+        }
+
+        //Editor Only Code
+        private void OnDrawGizmos()
+        {
+            if (!_gameStarted && transform.hasChanged)
+            {
+                CurrentPosition = transform.position;
+            }
+
+            for (var index = 0; index < _points.Length; index++)
+            {
+                var currentPoint = _points[index] + CurrentPosition;
+
+                //Draw Gizmo Point
+                Gizmos.color = Color.green;
+                Gizmos.DrawWireSphere(currentPoint, 0.5f);
+
+                var lastIndex = _points.Length - 1;
+                if (index != lastIndex)
+                {
+                    //Draw Gizmo Lines between points
+                    Gizmos.color = Color.gray;
+                    var nextPoint = _points[index + 1] + CurrentPosition;
+                    Gizmos.DrawLine(currentPoint, nextPoint);
+                }
+            }
+        }
+    }
+}
