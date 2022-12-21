f823eee10d8b53470ab825cb5c6dc009920b0989
Rasmus Wulff Jensen <rasmus.wulff.jensen@outlook.com>
2022-12-21 13:57:11 +0100

    Ball Launcher done

--- Ball Launcher/Assets/Scripts/BallHandler.cs
+++ Ball Launcher/Assets/Scripts/BallHandler.cs
@@ -1,26 +1,42 @@
+using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class BallHandler : MonoBehaviour
 {
     [SerializeField] private GameObject _ballPrefab;
     [SerializeField] private Rigidbody2D _pivot;
     [SerializeField] private float _respawnTime;
+    [SerializeField] private TextMeshProUGUI _youWonText;
+    [SerializeField] private TextMeshProUGUI _shotsText;
 
     private Camera _camera;
     private Rigidbody2D _currentBallRidgidBody;
     private SpringJoint2D _currentBallSpringJoint;
     private bool _isDragging;
+    private bool _won;
+    private int _numberOfShots;
 
     private void Start()
     {
+        _numberOfShots = 0;
         _camera = Camera.main;
         StartCoroutine(SpawnNewBall(0));
     }
 
     private void Update()
     {
+        UpdateShotsText();
+
+        if (_won)
+        {
+            return;
+        }
+
+        CheckWinCondition();
         if (_currentBallRidgidBody == null)
         {
             return;
@@ -43,6 +59,26 @@ public class BallHandler : MonoBehaviour
         _currentBallRidgidBody.position = worldPosition;
     }
 
+    private void UpdateShotsText()
+    {
+        _shotsText.text = "Short" + Environment.NewLine + _numberOfShots;
+    }
+
+    private void CheckWinCondition()
+    {
+        var towerBlocks = GameObject.FindGameObjectsWithTag("TowerBlock");
+        foreach (var towerBlock in towerBlocks)
+        {
+            if (IsTargetVisible(towerBlock))
+            {
+                return;
+            }
+        }
+
+        //we end here is none of the towerblocks are visisble
+        _youWonText.gameObject.SetActive(true);
+    }
+
     private IEnumerator SpawnNewBall(float delay)
     {
         yield return new WaitForSeconds(delay);
@@ -52,6 +88,7 @@ public class BallHandler : MonoBehaviour
         _currentBallRidgidBody = newBall.GetComponent<Rigidbody2D>();
         _currentBallSpringJoint = newBall.GetComponent<SpringJoint2D>();
         _currentBallSpringJoint.connectedBody = _pivot;
+        _numberOfShots++;
     }
 
     private void LaunchBall()
@@ -69,5 +106,22 @@ public class BallHandler : MonoBehaviour
         yield return new WaitForSeconds(delay);
         _currentBallSpringJoint.enabled = false;
         _currentBallSpringJoint = null;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private bool IsTargetVisible(GameObject go)
+    {
+        var planes = GeometryUtility.CalculateFrustumPlanes(_camera);
+        var point = go.transform.position;
+        foreach (var plane in planes)
+        {
+            if (plane.GetDistanceToPoint(point) < 0)
+                return false;
+        }
+        return true;
     }
 }
