90b36c6933eeabcf954ac9da04408db28f4b229b
Rasmus Wulff Jensen <rasmus.wulff.jensen@outlook.com>
2022-12-24 16:55:23 +0100

    Go

--- BasicConcepts/Assets/Scripts/MyComponent.cs
+++ BasicConcepts/Assets/Scripts/MyComponent.cs
@@ -7,12 +7,13 @@ public class MyComponent : MonoBehaviour
     private SpriteRenderer _spriteRenderer;
     private BasicConcepts _basicConcepts;
     [SerializeField] private float _moveSpeed;
+    private Rigidbody2D _rigidBody;
 
     // Start is called before the first frame update
     void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        
+        _rigidBody = GetComponent<Rigidbody2D>();
         //Option 3
         _basicConcepts = new BasicConcepts();
         _basicConcepts.Player.Enable();
@@ -42,9 +43,13 @@ public class MyComponent : MonoBehaviour
             SetColor();
         }*/
 
-        var move = (Vector3)_basicConcepts.Player.Move.ReadValue<Vector2>();
-        //transform.position += _move * _moveSpeed;
-        GetComponent<Rigidbody2D>().velocity = move * _moveSpeed;
+        Move();
+    }
+
+    private void Move()
+    {
+        var move = _basicConcepts.Player.Move.ReadValue<Vector2>();
+        _rigidBody.velocity = move * _moveSpeed;
     }
 
     private void SetColor()
